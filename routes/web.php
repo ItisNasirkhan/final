d7a967f904408a7e280e10ca562d6966f319eeaf
ItisNasirkhan <nk.102030g@gmail.com>
2024-12-25 17:54:51 +0500

    project migrated

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,25 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.index_content');
 });
+
+
+
+Route::get('/index', [HomeController::class, 'index']);
+Route::get('/index', [HomeController::class, 'index_content']);
+Route::get('/shop', [PagesController::class, 'shop']);
+Route::get('/shop', [PagesController::class, 'shop_content']);
+Route::get('/contact', [PagesController::class, 'contact']);
+Route::get('/contact', [PagesController::class, 'contact_content']);
+
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
